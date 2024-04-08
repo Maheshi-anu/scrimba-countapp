48b7f6b363487a3ad019691a8cb21efd1c666ec6
Maheshi Anuradha <maheshianuradha2000@gmail.com>
2024-04-08 22:31:37 +0530

    updated code

--- index.js
+++ index.js
@@ -54,23 +54,6 @@
 
 // console.log(lapsCompleted)
 
-let countEl = document.querySelector(".count-elem")
-console.log(countEl)
-
-let count = 0
-
-function increment(){
-    count = count + 1
-    countEl.innerText = count
-    console.log(count)
-}
-
-function save(){
-    console.log(count)
-}
-
-save()
-
 // let userName = "John"
 // console.log(userName)
 
@@ -89,4 +72,31 @@ save()
 // let bonusPoint = 10
 // let totalPoints = points + bonusPoint
 // console.log(totalPoints)
+
+
+let saveEl = document.querySelector("#save-el")
+let countEl = document.querySelector(".count-elem")
+console.log(countEl)
+
+let count = 0
+
+function increment(){
+    // count = count + 1
+    count += 1 //same as above
+    countEl.textContent = count
+    console.log(count)
+}
+
+function save(){
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+
+    console.log(count)
+}
+
+save()
+
+
 
